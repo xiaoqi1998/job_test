707119ed86ab6daacd1c782d097a69f58b3e4cf6
xiaopo1998 <1223527522@qq.com>
2023-07-14 16:43:16 +0800

    2023/7/14提交练习代码

--- 异常处理.py
+++ 异常处理.py
@@ -1,11 +1,12 @@
-#写一个判断获取商
-def Seeking_business(x,y):
+# 写一个判断获取商
+def seeking_business(x, y):
     try:
-        sum=x/y
+        sum = x / y
         print(sum)
-    except ZeroDivisionError:
-        print('0不能被除')
+    except Exception:
+        print('报错了')
 
-x=int(input('输入除数'))
-y=int(input('输入被除数'))
-Seeking_business(x,y)
+
+x = int(input('输入除数'))
+y = int(input('输入被除数'))
+seeking_business(x, y)
