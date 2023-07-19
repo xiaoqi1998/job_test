b9ac7226152cff0aa0d5d696fab662aefad7d408
xiaopo1998 <1223527522@qq.com>
2023-07-19 18:01:05 +0800

    2023/7/19 加了一点发起请求的联系

--- 请求练习/get.py
+++ 请求练习/get.py
@@ -5,7 +5,9 @@ url = 'https://www.mxnzp.com/api/daily_word/recommend'
 key = {'app_id': 'xormnrfmlu8ku9sb', 'app_secret': 'FIv7MkheiglAjHjU2YHNpNR5EZ5T08Xg', 'idcard': '513822199803402345',
        'encoding': 'utf-8'}
 res = requests.get(url, key)  # 输入地址和请求参数
-print('url', res.request.url)  # 获取请求的地址
 reszult = res.json()  # 获取
+status = res.status_code
+header = res.headers
 reszult = reszult['data'][0]['content']
-print('返回的参数', reszult)
+print('请求连接：%s \n请求头：%s \n  请求状态：%s  \n 返回结果%s' % (url, header, status, reszult))
+res.close()  # 断开连接
